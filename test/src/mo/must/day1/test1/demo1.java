819487ab73a9646154718e2b45ba2cfa249972bc
GUO ZEXIN <a1025922331@163.com>
2020-10-19 20:17:51 +0800

    2020/10/19 version 1.0.1

--- test/src/mo/must/day1/test1/demo1.java
+++ test/src/mo/must/day1/test1/demo1.java
@@ -1,13 +1,12 @@
 package mo.must.day1.test1;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class demo1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String s = sc.nextLine();
-        int i = sc.nextInt();
-        System.out.println(s);
-        System.out.println(i);
-
+        ArrayList a = new ArrayList();
+        a.add(sc.nextInt());
+        System.out.println(a.get(0).getClass());
     }
 }
