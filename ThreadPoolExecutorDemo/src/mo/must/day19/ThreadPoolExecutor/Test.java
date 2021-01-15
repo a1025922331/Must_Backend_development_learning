a53e4b3e5cac3635f65243c84e8473f2b8f2ad51
GUO ZEXIN <a1025922331@163.com>
2021-01-16 00:25:47 +0800

    2021/01/16 version 19.0.0

--- ThreadPoolExecutorDemo/src/mo/must/day19/ThreadPoolExecutor/Test.java
+++ ThreadPoolExecutorDemo/src/mo/must/day19/ThreadPoolExecutor/Test.java
@@ -1,9 +1,6 @@
 package mo.must.day19.ThreadPoolExecutor;
 
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 public class Test {
     public static void main(String[] args) {
@@ -20,17 +17,21 @@ public class Test {
 //                                                                        new ThreadPoolExecutor.CallerRunsPolicy());
         for (int i = 1; i <= 10; i++) {
             final int x = i; //使用最终变量来完成内部类对本地变量的间接引用
-            threadPoolExecutor.submit(new Runnable() {
-                @Override
-                public void run() {
-                    try {
-                        Thread.sleep(100); //等待一会，保证线程不至于那么快运行完
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
+            try {
+                threadPoolExecutor.submit(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            Thread.sleep(100); //等待一会，保证线程不至于那么快运行完
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        System.out.println(Thread.currentThread().getName()+"线程正在执行任务" + x);
                     }
-                    System.out.println(Thread.currentThread().getName()+"线程正在执行任务" + x);
-                }
-            });
+                });
+            } catch (RejectedExecutionException e) {
+                System.out.println("捕获到RejectedExecutionException异常：任务"+ i + "被线程池拒绝了！");
+            }
         }
         //关闭线程池
         threadPoolExecutor.shutdown();
